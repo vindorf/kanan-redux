f2c982297779f1c4c029d3406f56aa93fc2b86e0
321go <gehgo2486@yahoo.com>
2024-03-29 13:29:25 +0100

    add_git_auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-
+"use client"
+import { signOut } from "next-auth/react";
 
 export default function Home() {
   return (
     <main>
       <p>Hi</p>
+      <button onClick={() => signOut()}>Logout</button>
     </main>
   );
 }
