9c533b37e05edba0cf2ac4a28f1495f9e76dc782
321go <gehgo2486@yahoo.com>
2024-03-30 11:46:49 +0100

    add_api

---
+++ src/redux/services/apiSlice.ts
@@ -0,0 +1,31 @@
+import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
+import { collection, getDocs } from "firebase/firestore";
+import { getSession } from "next-auth/react";
+import { db } from "../../app/utils/firebaseConfig";
+
+
+export const fireStoreApi = createApi({
+    reducerPath: "firestoreApi", 
+    baseQuery: fakeBaseQuery(), 
+    tagTypes: ["Tasks"], 
+    endpoints: (builder) => ({
+     fetchDataFromDb: builder.query<{ [key: string]: any }[], void>({
+       async queryFn() {
+ 
+         try {
+           const session = await getSession();
+           const { user } = session!;
+             const ref = collection(db, `users/${user?.email}/tasks`);
+             const querySnapshot = await getDocs(ref);
+             return { data: querySnapshot.docs.map((doc) => doc.data()) };
+           
+         } catch (e) {
+           return { error: e };
+         }
+       },
+       providesTags: ["Tasks"], 
+     }),
+    }),
+    });
+ 
+    export const { useFetchDataFromDbQuery } = fireStoreApi;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-//import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { rootReducer } from "./rootReducer";
+import { fireStoreApi } from "./services/apiSlice";
 
-// Create the Redux store
 export const store = configureStore({
-  reducer: {}, // Add your reducers here
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(fireStoreApi.middleware),
 });
 
-// Setup listeners for refetch behaviors
 setupListeners(store.dispatch);
 
-// Define RootState and AppDispatch types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
