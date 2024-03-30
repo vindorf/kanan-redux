8d49a678e3f727243a0227a719dcf84a3e7191ca
321go <gehgo2486@yahoo.com>
2024-03-30 08:14:45 +0100

    google_cred

--- src/app/utils/firebaseConfig.ts
+++ src/app/utils/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyCX5784R5ucwPCPYoatZFCDmDGu43ynp-s",  
-    authDomain: "kanan-b49d1.firebaseapp.com",  
-    projectId: "kanan-b49d1",  
-    storageBucket: "kanan-b49d1.appspot.com",  
-    messagingSenderId: "942029116883",  
-    appId: "1:942029116883:web:d2122a16c803d74198cb26",  
-    measurementId: "G-JFQZH3PZ5R"
+    apiKey: process.env.API_KEY,  
+    authDomain: process.env.AUTH_DOMAIN,  
+    projectId: process.env.PROJECT_ID,  
+    storageBucket: process.env.BUCKET,  
+    messagingSenderId: process.env.SENDER_ID,  
+    appId: process.env.APP_ID,  
+    measurementId: process.env.MEASUREMENT_ID
   
   };
   
