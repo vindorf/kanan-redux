e7451cbff71acfc29972d89787aa797a63c5a3fe
321go <gehgo2486@yahoo.com>
2024-03-31 19:19:38 +0200

    add_tasks

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,18 +1,52 @@
 
+import { useState } from "react";
+import { useAppDispatch } from "../../redux/hooks";
+import { useFetchDataFromDbQuery } from "../../redux/services/apiSlice";
+import { setPageTitle } from "../../redux/features/appSlice";
+
 export default function Sidebar() {
-    return (
-     <aside className="w-[18.75rem] flex-none dark:bg-dark-grey h-full py-6 pr-6">
-       <p className="text-medium-grey pl-[2.12rem] text-[.95rem] font-semibold uppercase pb-3">
-         {`All Boards (0)`}
-       </p>
-       <div className="cursor-pointer flex items-center rounded-tr-full rounded-br-full bg-blue-500 space-x-2 pl-[2.12rem] py-3 pb-3">
-         <p className="text-white text-lg capitalize">Current board name</p>
-       </div>
-       <button className="flex items-center space-x-2 pl-[2.12rem] py-3">
-         <p className="text-base font-bold capitalize text-main-purple">
-           + Create New Board
-         </p>
-       </button>
-     </aside>
-    );
+  const [active, setActive] = useState<number>(0);
+  const { data } = useFetchDataFromDbQuery();
+  const dispatch = useAppDispatch();
+
+  const handleNav = (index: number, name: string) => {
+    setActive(index);
+    dispatch(setPageTitle(name));
+  };
+  return (
+    <aside className="w-[18.75rem] flex-none dark:bg-dark-grey h-full py-6 pr-6">
+      {data && (
+        <>
+          {/* Display the number of boards available in the data */}
+          <p className="text-medium-grey pl-[2.12rem] text-[.95rem] font-semibold uppercase pb-3">
+            {`All Boards (${data[0]?.boards.length})`}
+          </p>
+          {/* Display the names of each board */}
+          {data[0]?.boards.map(
+            (board: { [key: string]: any }, index: number) => {
+              const { name, id } = board;
+              const isActive = index === active; // Check if the board is active
+              return (
+                <div
+                  key={id}
+                  onClick={() => handleNav(index, name)} // Handle navigation through boards on click
+                  className={`${
+                    isActive ? 'rounded-tr-full rounded-br-full bg-blue-500 text-white' : 'text-black'
+                  } cursor-pointer flex items-center 
+                  space-x-2 pl-[2.12rem] py-3 pb-3`}
+                >
+                  <p className="text-lg capitalize">{name}</p>
+                </div>
+              );
+            }
+          )}
+        </>
+      )}
+      <button className="flex items-center space-x-2 pl-[2.12rem] py-3">
+        <p className="text-base font-bold capitalize text-main-purple">
+          + Create New Board
+        </p>
+      </button>
+    </aside>
+  );
     }
