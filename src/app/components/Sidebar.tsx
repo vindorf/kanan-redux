5a86c0e2107a287f21c9caa5f0fb4aeb3e86dc6f
321go <gehgo2486@yahoo.com>
2024-04-02 10:29:17 +0200

    add_mod_newBoard

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { useAppDispatch } from "../../redux/hooks";
 import { useFetchDataFromDbQuery } from "../../redux/services/apiSlice";
-import { setPageTitle } from "../../redux/features/appSlice";
+import { openAddAndEditBoardModal, setPageTitle } from "../../redux/features/appSlice";
 
 export default function Sidebar() {
   const [active, setActive] = useState<number>(0);
@@ -17,19 +17,17 @@ export default function Sidebar() {
     <aside className="w-[18.75rem] flex-none dark:bg-dark-grey h-full py-6 pr-6">
       {data && (
         <>
-          {/* Display the number of boards available in the data */}
           <p className="text-medium-grey pl-[2.12rem] text-[.95rem] font-semibold uppercase pb-3">
             {`All Boards (${data[0]?.boards.length})`}
           </p>
-          {/* Display the names of each board */}
           {data[0]?.boards.map(
             (board: { [key: string]: any }, index: number) => {
               const { name, id } = board;
-              const isActive = index === active; // Check if the board is active
+              const isActive = index === active; 
               return (
                 <div
                   key={id}
-                  onClick={() => handleNav(index, name)} // Handle navigation through boards on click
+                  onClick={() => handleNav(index, name)} 
                   className={`${
                     isActive ? 'rounded-tr-full rounded-br-full bg-blue-500 text-white' : 'text-black'
                   } cursor-pointer flex items-center 
@@ -42,7 +40,9 @@ export default function Sidebar() {
           )}
         </>
       )}
-      <button className="flex items-center space-x-2 pl-[2.12rem] py-3">
+      <button 
+      onClick={() => dispatch(openAddAndEditBoardModal('Add new Board'))}
+      className="flex items-center space-x-2 pl-[2.12rem] py-3">
         <p className="text-base font-bold capitalize text-main-purple">
           + Create New Board
         </p>
